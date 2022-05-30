a4f2a5db3ca37064a2e323f3f4f13dcba681a674
EladShmulevich <eladshmu@post.bgu.ac.il>
2022-05-30 13:07:22 +0300

    afas

--- src/IO/MyCompressorOutputStream.java
+++ src/IO/MyCompressorOutputStream.java
@@ -10,6 +10,8 @@ public class MyCompressorOutputStream extends OutputStream {
         this.out = os;
     }
 
+    int x = 66;
+
     @Override
     public void write(int b) throws IOException {
         try{
