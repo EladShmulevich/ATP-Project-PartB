c17c07098c31f68d841bc9d48a305751c21df184
EladShmulevich <eladshmu@post.bgu.ac.il>
2022-06-03 18:17:43 +0300

    finished all compressors

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,4 +1,36 @@
 package Client;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Client {
+    private InetAddress serverIP;
+    private int serverPort;
+    private IClientStrategy strategy;
+
+
+
+    public Client(InetAddress serverIP, int serverPort, IClientStrategy strategy) {
+        this.serverIP = serverIP;
+        this.serverPort = serverPort;
+        this.strategy = strategy;
+    }
+
+    public void start(){
+        try{
+            Socket clientSocket = new Socket(serverIP, serverPort);
+            //System.out.println("connected to server - IP = " + serverIP + ", Port = " + serverPort);
+            strategy.clientStrategy(clientSocket.getInputStream(),clientSocket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
 }
