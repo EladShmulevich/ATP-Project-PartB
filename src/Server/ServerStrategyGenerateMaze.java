29d9f96a4ceb333730db212c40ded7cca55384e2
Ido Lourie <63515984+idolou@users.noreply.github.com>
2022-06-03 17:07:23 +0300

    simple compress + decompress

--- src/Server/ServerStrategyGenerateMaze.java
+++ src/Server/ServerStrategyGenerateMaze.java
@@ -1,11 +1,32 @@
 package Server;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-
+import IO.MyCompressorOutputStream;
+import algorithms.mazeGenerators.IMazeGenerator;
+import algorithms.mazeGenerators.Maze;
+import java.io.*;
 public class ServerStrategyGenerateMaze implements IServerStrategy{
     @Override
-    public void ServerStrategy(InputStream inFromClient, OutputStream outToClient) {
+    public void ServerStrategy(InputStream inFromClient, OutputStream outToClient) throws IOException, ClassNotFoundException {
+        try{
 
+            ObjectInputStream fromClient = new ObjectInputStream(inFromClient);
+            ObjectOutputStream toClient = new ObjectOutputStream(outToClient);
+
+            int[] mazeDimensions = (int[])fromClient.readObject();
+
+            IMazeGenerator mazeGenerator = Configurations.generateMazeAlgorithmConfig();
+            Maze newMaze = mazeGenerator.generate(mazeDimensions[0], mazeDimensions[1]);
+
+            ByteArrayOutputStream outClient = new ByteArrayOutputStream();
+
+            MyCompressorOutputStream compressedOutput  = new MyCompressorOutputStream(outClient);
+            compressedOutput.write(newMaze.toByteArray());
+
+            toClient.writeObject(outClient.toByteArray());
+
+            toClient.flush();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
